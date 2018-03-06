f81315d5a16f5966aa68a9ea98a2583b33503035
hdmvide <hdmcomputer@gmail.com>
2018-03-06 09:23:11 +0100

    Add files via upload

--- jquery.mscaletextarea.js
+++ jquery.mscaletextarea.js
@@ -14,31 +14,31 @@
     }
   };
 
-  function txtHandler(handleObj) {
-		
-		var origHandler = handleObj.handler;
-		
+  function txtHandler(event) {
+
+        var handleObj = event.handleObj;
+        var origHandler = handleObj.handler;
+
 		$(this).on("mouseup", function(e) {
 				if ($.mScaleTextarea.options.resizeInt !== null) {
 						clearInterval($.mScaleTextarea.options.resizeInt);
 				}
 		});
 
-		handleObj.handler = function(event) {
+		$(this).on("mousedown", function(e) {
 			$.mScaleTextarea.options.resizeInt = setInterval(function(){
 				return origHandler.apply(event.target, arguments);
 			}, 1000/15);
-		}
+		});
 		
   }
-
-	$.event.special.mousedown = {
-		add: txtHandler
-	};
 	
 	$.event.special.mScaleTextarea = {
 			bindType: "mousedown",
-			delegateType: "mousedown"
+			delegateType: "mousedown",
+            handle: function( event ) {
+                txtHandler(event);
+            }
 	};
 
 })(jQuery || this.jQuery || window.jQuery);
